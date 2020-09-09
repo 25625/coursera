c4d17f6dd663e9ba867aecfbcfb4907fa807e0e7
25625 <31729388+25625@users.noreply.github.com>
2020-09-09 15:30:08 +0530

    Update app.js

--- Anjularjs/coursera/app.js
+++ Anjularjs/coursera/app.js
@@ -4,22 +4,23 @@
 	.controller('Control',function($scope){
 		//$scope.name = "";
 		$scope.hello = function(){
+			$scope.message = "hi"
 			var str = $scope.name;
 			var words = str.split(',');
 			$scope.test1 = words.length;
 			if($scope.name == "")
 			{
-				return "Please enter data first";
+				$scope.message = "Please enter data first";
 			}
 			else {
 			if (words.length >= 1 && words.length <= 3) {
-					return "Enjoy!";
+					$scope.message = "Enjoy!";
 			}
 			else if (words.length == 0 || $scope.name=="n") {
-					return "Please enter data first";
+					$scope.message = "Please enter data first";
 			}
 			else if (words.length>3) {
-					return "Too much!";
+					$scope.message = "Too much!";
 			}
 		}
 
